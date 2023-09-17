61024cc1cc644d496a609b2252213549c6ea44ab
Nicolas Barbosa <nicobarbosa20034@gmail.com>
2023-09-17 19:05:43 -0300

    separando componentes e adicionando pastas

---
+++ components/Login.tsx
@@ -0,0 +1,52 @@
+import { StatusBar } from 'expo-status-bar';
+import { View, Button, TextInput, Alert, Image } from 'react-native';
+import React, { useState } from 'react';
+import { styles } from './styles';
+
+
+export default function Login() {
+
+    const [usuario, setUsuario] = useState('');
+    const [senha, setSenha] = useState('');
+
+    return(
+        <View 
+            style={styles.container}>
+            <Image 
+                style={styles.logo} 
+                source={require('../assets/logo.png')}
+            />
+            <TextInput 
+                onChangeText={setUsuario} 
+                style = {styles.input} 
+                placeholder='Usuário'
+            />
+            <TextInput 
+                onChangeText={setSenha} 
+                secureTextEntry={true} 
+                style = {styles.input} 
+                placeholder='Senha'
+            />
+            <Button
+                title="Login" 
+                color="#000000"
+                onPress={() => {
+
+                if(usuario === 'admin' && senha === '1234'){
+                    Alert.alert('Login efetuado com sucesso!')
+                    return
+                }
+                
+                if(usuario === '' || senha === ''){
+                    Alert.alert('Preencha todos os campos!')
+                    return
+                }
+                else{
+                    Alert.alert('Usuário ou senha incorreto!')
+                }
+                }}
+            />
+            <StatusBar style="auto" />
+        </View>
+    );
+}
---
+++ components/styles.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+
+    container: {
+      flex: 1,
+      backgroundColor: '#ff8c00',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      marginTop: 40,
+      resizeMode: 'cover'
+    },
+  
+    input: {
+      height: 40,
+      width: 200,
+      marginBottom: 20,
+      borderWidth: 1,
+      padding: 10,
+      backgroundColor: '#fff',
+    },
+  
+    titleText: {
+      fontSize: 20,
+      fontWeight: 'bold',
+      margin: 20,
+    },
+
+    logo: {
+      width: 250,
+      height: 250,
+      resizeMode: 'contain',
+      margin: 50,
+    }
+  });
