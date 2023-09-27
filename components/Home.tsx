bcfcefa3c7f05a4e1bf5dcc60d4865e2cab6cf8c
joycemcarvalho12 <joyce.carvalho125@hotmail.com>
2023-09-27 20:15:54 -0300

    atualizado

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,14 +1,43 @@
-import { View } from "react-native"
+import { ScrollView } from "react-native"
 import { Text } from "@rneui/themed"
-import { Button } from "@rneui/base"
+import { Button, Divider } from "@rneui/base"
+import { useEffect, useState } from "react"
+import axiosConfig from "../config/axios"
+import { ListItem } from "react-native-elements"
+import { ListItemContent } from "@rneui/base/dist/ListItem/ListItem.Content"
+import { ListItemTitle } from "@rneui/base/dist/ListItem/ListItem.Title"
 
-export default function Home({ navigation }){
+export default function Home ({ navigation}) {
+    const [produtos, setProdutos] = useState([])
+
+    useEffect (() => {
+        axiosConfig.get("/products").then((Response) => {
+            setProdutos(Response.data.products)
+    })
+}, [])
     return(
-        <View>
+        <ScrollView>
             <Text h1>Home</Text>
             <Button title='Sair' onPress={
                 () => navigation.navigate('Login')
             } />
-        </View>
-    )
+            <Text h3>Produtos</Text>
+            <Divider />
+            {
+                produtos.length <= 0 && (
+                    <Text> Nenhum produto encontrado</Text>
+                )
+            }
+            {produtos.map((produto) =>
+            (
+                <ListItem key={produto.id}>
+                    <ListItemContent>
+                        <ListItemTitle>
+                            <Text>{produto.title}</Text>
+                        </ListItemTitle>
+                    </ListItemContent>
+                </ListItem>
+            ))}
+        </ScrollView>
+        )
 }
